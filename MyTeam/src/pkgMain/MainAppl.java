c10ad85e6fd71fa34cd7e499659eab16f898873d
htlvil-org <org@htl-villach.at>
2021-06-18 11:21:11 +0200

    Update MainAppl.java

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,6 +7,8 @@ public class MainAppl {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
+		player = new Player(4, "Hinteregger", "Defense");
+		System.out.println("== " + player);
 	}
 
 }
