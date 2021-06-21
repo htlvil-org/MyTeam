020a6c2b98cb3949bb3d8267ee44d2e6ea0ec5dd
htlvil-org <org@htl-villach.at>
2021-06-21 15:42:42 +0200

    2nd oldies out

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -6,17 +6,7 @@ public class MainAppl {
 
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
-		System.out.println("== " + player);
-		player = new Player(4, "Hinteregger", "Defense");
-		System.out.println("== " + player);
-		player = new Player(5, "Alaba", "Defense");
-		System.out.println("== " + player);
 
-		player = new Player(10, "Kalajdzic", "Striker");
-		System.out.println("== " + player);
-		player = new Player(11, "Arnautovic", "Striker");
-
-		System.out.println("== " + player);
 		
 		player = new Player(8, "Baumgartner", "Midfielder");
 		System.out.println("== " + player);
