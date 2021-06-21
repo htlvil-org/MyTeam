1f9a02f493cef4c2aec4517a3c39813198d8d47c
schueler <schueler@ubuntu>
2021-06-21 15:39:09 +0200

    old player out

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -5,14 +5,9 @@ import pkgData.Player;
 public class MainAppl {
 
 	public static void main(String[] args) {
-		Player player = new Player(2, "Dragovic", "Defense");
-		System.out.println("== " + player);
-		player = new Player(4, "Hinteregger", "Defense");
-		System.out.println("== " + player);
-		player = new Player(5, "Alaba", "Defense");
-		System.out.println("== " + player);
 
-		player = new Player(10, "Kalajdzic", "Striker");
+
+		Player player = new Player(10, "Kalajdzic", "Striker");
 		System.out.println("== " + player);
 		player = new Player(11, "Arnautovic", "Striker");
 
